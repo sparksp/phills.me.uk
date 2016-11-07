d08859109ca66f5af242861d1532b70361f7d89a
Phill Sparks <me@phills.me.uk>
2016-11-07 15:23:10 +0000

    Parse for redirects in the given file

---
+++ redirect-parser.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"golang.org/x/net/html"
+)
+
+type Redirect struct {
+	from string
+	to   string
+}
+
+func (redirect Redirect) String() string {
+	return redirect.from + " " + redirect.to
+}
+
+func getCanonical(t html.Token) (href string, ok bool) {
+	for _, a := range t.Attr {
+		switch a.Key {
+		case "rel":
+			if a.Val == "canonical" {
+				ok = true
+			} else {
+				return "", false
+			}
+		case "href":
+			href = a.Val
+		}
+	}
+	return
+}
+
+func pathIndex(filename string) string {
+	if path.Base(filename) != "index.html" {
+		return filename
+	}
+	return path.Dir(filename) + "/"
+}
+
+func crawl(filename string, chRedirect chan Redirect, chFinished chan bool) {
+	defer func() {
+		chFinished <- true
+	}()
+	var (
+		from = pathIndex(filename)
+		to   string
+	)
+	defer func() {
+		if from != "" && to != "" {
+			chRedirect <- Redirect{from, to}
+		}
+	}()
+
+	b, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("ERROR: Failed to crawl \"" + filename + "\"")
+		return
+	}
+	z := html.NewTokenizer(b)
+
+	for {
+		tt := z.Next()
+
+		switch tt {
+		case html.ErrorToken:
+			return
+		case html.SelfClosingTagToken:
+			t := z.Token()
+
+			switch t.Data {
+			case "link":
+				if url, ok := getCanonical(t); ok {
+					to = url
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	chRedirect := make(chan Redirect)
+	chFinished := make(chan bool)
+
+	searchFiles := os.Args[1:]
+
+	for _, filename := range searchFiles {
+		go crawl(filename, chRedirect, chFinished)
+	}
+
+	for c := 0; c < len(searchFiles); {
+		select {
+		case redirect := <-chRedirect:
+			fmt.Println(redirect)
+		case <-chFinished:
+			c++
+		}
+	}
+}
